36493272ed6f04d2eb27b673aeac14834d5282d8
Omar De Los Santos <omar.dlsg@gmail.com>
2018-05-05 16:14:52 -0700

    Set up app.jsx

---
+++ frontend/app.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function Root() {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    ReactDOM.render(<Root />, document.getElementById('root'));
+});
