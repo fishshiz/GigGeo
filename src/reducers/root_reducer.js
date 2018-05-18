082a961ad9ff64fefeadbfd265041d7292d4674e
Edan Lewis <edan@lewisarts.com>
2018-05-18 15:51:44 -0700

    Add init redux skeleton

---
+++ src/reducers/root_reducer.js
@@ -0,0 +1,6 @@
+import { combineReducers } from "redux";
+import venues from "./venues_reducer";
+
+export default combineReducers({
+    venues
+});
---
+++ src/store/store.js
@@ -0,0 +1,16 @@
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+import rootReducer from '../reducers/root_reducer';
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+
+const configureStore = (preloadedState = {}) => (
+    createStore(
+        rootReducer,
+        preloadedState,
+        composeEnhancers(applyMiddleware(thunk, logger))
+    )
+);
+
+export default configureStore;
